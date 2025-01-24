922f3423924197f66b1b0a36b7fdac2d708a5404
Savazh <your_email@example.com>
2025-01-24 22:54:53 +0200

    v1

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AnalyticsDashboard from "./components/AnalyticsDashboard";
+import AddressLookup from "./components/AddressLookup";
+function App() {
+  return (
+    <div>
+      <AddressLookup />
+      <AnalyticsDashboard />
+    </div>
+  );
+}
+export default App;
